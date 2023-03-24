57d861368106445f7ef89bb76fc35e987850caa2
777may <mayri.luli@gmail.com>
2023-03-24 18:48:51 -0300

    cambios para copiar en cpu

---
+++ tp2_1_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<time.h>
+
+#define N 20
+
+void main(){
+    int i;
+    double vt[N];
+    srand(time(NULL));
+    for(i = 0;i<N; i++){
+        vt[i]=1+rand()%100;
+        printf("%f ", vt[i]);
+    }
+
+
+
+
+}
